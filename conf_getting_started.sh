35072b32d3dbf6bb9fee70e44d3b2d1a38603650
akervadec <akervadec@linagora.com>
2017-05-23 00:21:09 +0200

    Initial config for Debian9 with Gnome3. TODO: installation of KVM/Qemu, Docker, ssh-server rsync openvpn

--- conf_getting_started.sh
+++ conf_getting_started.sh
@@ -1,4 +1,7 @@
-#/bin/sh
+#!/bin/bash
+########################################
+### Tested for Debian 9 with Gnome 3 ###
+########################################
 
 # Great tuto to install and configure openbox : https://www.maketecheasier.com/configure-andcustomize-openbox/
 
@@ -9,28 +12,39 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install  openbox obconf git build-essential software-properties-common zip terminator gmrun tilda nitrogen -y
+# Graphical environment configuration
 
-# Menu conf
-sudo apt-get install menu -y
-cp /var/lib/openbox/debian-menu.xml ~/.config/openbox/debian-menu.xml
-cp /etc/xdg/openbox/menu.xml ~/.config/openbox/menu.xml
-cp /etc/xdg/openbox/rc.xml ~/.config/openbox/rc.xml
-openbox --reconfigure
+sudo apt-get install vim hubic build-essential software-properties-common zip terminator gmrun tilda bzip2 redshift wget curl ldap-utils dnsutils -y
 
-sudo apt-get install obmenu -y
-obmenu
-# At this point, personalize the menu (or not), then close the window
+echo "Choose /usr/bin/vim/basic, or die"
+sudo update-alternatives --config editor
 
-sudo apt-get install tint2 -y
-cd ~/.config
-rm -rvf tint2
-git clone https://github.com/RiceBowlJr/tint2.git
+cp config/autostart/*.desktop ~/.config/autostart/
+cp config/redshift.conf ~/.config/
+cp .vimrc ~/
 
-rm -rvf openbox
-git clone https://github.com/RiceBowlJr/openbox.git
+# Installation and configuration of other softwares
 
-wget https://dl.opendesktop.org/api/files/download/id/1485941697/Mistral-Thin-Dark.obt
+cd
+mkdir -p ~/apps
+mkdir -p ~/apps/deb
+
+# Firefox
+cd ~/apps
+wget -O 'firefox-54.0a2.en-US.linux-x86_64.tar.bz2' 'https://download.mozilla.org/?product=firefox-aurora-latest-ssl&os=linux64&lang=en-US'
+cd /opt
+tar xjf firefox-54.0a2.en-US.linux-x86_64.tar.bz2
+sudo ln -s /opt/firefox/firefox /usr/bin/firefox
+
+# Cryptomator
+cd ~/apps/deb
+wget -O 'cryptomator-1.2.3-amd64.deb' 'https://bintray.com/cryptomator/cryptomator-deb/download_file?file_path=cryptomator-1.2.3-amd64.deb'
+sudo dpkg -i cryptomator-1.2.3-amd64.deb
+
+# Atom graphical text editor
+cd ~/apps/deb
+wget -O 'atom-amd64.deb' 'https://atom.io/download/deb'
+sudo dpkg -i atom-amd64.deb
 
 # From there, add the new theme in obconf, the obt file is in the openbonx conf directory
 wget http://i1.wp.com/abekislevitz.com/wp/wp-content/uploads/2013/10/OwlSpotting3.jpg
