adced34b7554471f2869aca002603c889853eb8e
tfabre <fabre.thibaut@gmail.com>
2017-06-01 12:01:12 +0200

    Add zsh scripts

---
+++ scripts/zsh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Fonction pour vérifier l'état de la commande précédente
+check() {
+    if [ $? = 0 ] ; then
+      echo "\nInstallation OK."
+      wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+      chsh -s `which zsh`
+      rsync $PWD/config/.zshrc ~/
+    else
+      echo "\nErreur lors de l'installation."
+    fi
+}
+
+echo "\n[INFO] Installation de vim..."
+sudo apt-get install -y zsh 1> /dev/null
+check
+
+exit 0
